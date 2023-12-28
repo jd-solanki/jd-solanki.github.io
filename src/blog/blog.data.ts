4cf8ebae7f5858345ee75195a64e0a1ac6c3e167
jd-solanki <jdsolanki0001@gmail.com>
2023-12-28 15:34:11 +0530

    fix: don't list index page in blog

--- src/blog/blog.data.ts
+++ src/blog/blog.data.ts
@@ -1,20 +1,14 @@
-import { createContentLoader } from 'vitepress';
-import { isDev } from '../../.vitepress/config';
+import { createContentLoader } from 'vitepress'
+import { isDev } from '../../.vitepress/config'
 
 export default createContentLoader('src/blog/*.md', {
-    transform(rawData) {
-        return rawData
-            .filter(item => (isDev ? true : !item.frontmatter.wip) && !item.url.endsWith('index.html'))
-            .map(item => {
-            // console.log(item);
-            
-            const r = {
-                ...item,
-                title: item.frontmatter.title + (item.frontmatter.wip && isDev ? ' 🚧' : ''),
-                url: item.url.replace(/^\/src/, ''),
-            }
-            
-            return r
-        })
-    },
+  transform(rawData) {
+    return rawData
+      .filter(item => (isDev ? true : !item.frontmatter.wip) && !item.url.endsWith('/index'))
+      .map(item => ({
+        ...item,
+        title: item.frontmatter.title + (item.frontmatter.wip && isDev ? ' 🚧' : ''),
+        url: item.url.replace(/^\/src/, ''),
+      }))
+  },
 })
