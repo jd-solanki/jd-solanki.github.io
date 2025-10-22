0ba8ebd813de28e2523e9bc0cbebd8d32fbb40b0
JD Solanki <jdsolanki0001@gmail.com>
2025-10-22 20:27:52 +0530

    fix(SEO): add site url for ensuring absolute url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   site: {
+    url: 'https://jd-solanki.github.io',
     name: 'JD Solanki',
   },
   content: {
