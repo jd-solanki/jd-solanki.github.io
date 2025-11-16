2094b1a79feccba9936621d36b5eaf2262922918
JD Solanki <jdsolanki0001@gmail.com>
2025-11-16 19:25:20 +0530

    perf: use `Promise.all` to wait for all async operations while fetching page

--- app/pages/blog/[slug].vue
+++ app/pages/blog/[slug].vue
@@ -4,13 +4,14 @@ import { withoutTrailingSlash } from 'ufo'
 const route = useRoute()
 const routePath = withoutTrailingSlash(route.path)
 
-const { data: post } = await useAsyncData(routePath, () => {
-  return queryCollection('blog').path(routePath).where('private', '<>', true).first()
-})
-
-const { data: surround } = await useAsyncData(`${routePath}-surround`, () => {
-  return queryCollectionItemSurroundings('blog', routePath).where('private', '<>', true)
-})
+const [{ data: post }, { data: surround }] = await Promise.all([
+  useAsyncData(routePath, () => queryCollection('blog').path(routePath).where('private', '<>', true).first()),
+  useAsyncData(`${routePath}-surround`, () => {
+    return queryCollectionItemSurroundings('blog', routePath, {
+      fields: ['description'],
+    }).where('private', '<>', true)
+  }),
+])
 
 if (!post.value) {
   throw createError({ statusCode: 404, statusMessage: 'Post not found' })
